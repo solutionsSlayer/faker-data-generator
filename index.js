b2ef870e91c0c0df03f721fb98ff3bb39e264464
dorian <ddoussain@gmail.com>
2022-04-29 13:07:03 +0300

    update data

--- index.js
+++ index.js
@@ -4,37 +4,43 @@ const fs = require('fs')
 function generateUsers() {
   let users = []
 
-  for (let id=1; id <= 100; id++) {
-
-    let address = faker.name.firstName();
-    let email = faker.name.lastName();
-    let password = faker.internet.email();
-    let created_at = faker.date.recent();
-
-    users.push({
-        "id": id,
-        address,
-        email,
-        password,
-        created_at
-    });
+  for (let id=1; id <= 10; id++) {
+    users.push({  'id': id, ...generateUser() });
   }
 
   return users;
 }
 
+function generateUser() {
+  let firstname = faker.name.firstName();
+  let lastname = faker.name.lastName();
+  let email = faker.internet.exampleEmail();
+  let thumbnail = faker.image.avatar();
+  let password = faker.internet.email();
+  let created_at = faker.date.recent();
+
+  return user = {
+      firstname,
+      lastname,
+      thumbnail,
+      email,
+      password,
+      created_at
+  };
+}  
+
 function generateCampaigns() {
   let campaigns = []
 
-  for (let id=1; id <= 100; id++) {
-
+  for (let id=1; id <= 10; id++) {
     let title = faker.company.bs();
+    let description = faker.company.catchPhrase();
     let thumbnail = faker.image.people() ;
     let currentAmount = faker.finance.amount(20000, 40000);
     let goalAmount = faker.finance.amount(100000, 100000);
     let duration = faker.date.future();
-    let description = faker.company.catchPhrase();
-    let location = faker.address.country();
+    let location = faker.address.country(); 
+    let comments = generateComments(); 
 
     campaigns.push({
         "id": id,
@@ -44,11 +50,32 @@ function generateCampaigns() {
         goalAmount,
         duration,
         description,
-        location
+        location,
+        comments
     });
   }
 
   return campaigns;
+}
+
+function generateComments() {
+  let comments = []
+
+  for (let id=1; id <= 10; id++) {
+    let content = faker.random.words(30);
+    let user = generateUser();
+
+    comments.push({
+        "id": id,
+        content,
+        user
+    });
+  }
+
+  return {
+    length: comments.length,
+    comments,
+  };
 }
 
 function generateData() {
